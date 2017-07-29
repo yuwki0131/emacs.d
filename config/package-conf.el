35db672c20f877ce0055c0aa060e21b1b57b0862
yuwki0131 <yuwki0131@gmail.com>
2017-07-30 04:09:33 +0900

    add exec-path-from-shell-initialize

--- config/package-conf.el
+++ config/package-conf.el
@@ -19,6 +19,13 @@
 (use-package-with-report magit)
 
 ;;; ---------------------------------------------------------------------------
+;;; package-func : magit : emacs git client
+;;; ---------------------------------------------------------------------------
+(use-package-with-report exec-path-from-shell
+  :config
+  (exec-path-from-shell-initialize))
+
+;;; ---------------------------------------------------------------------------
 ;;; package-func : server : emacs client/server
 ;;; ---------------------------------------------------------------------------
 (use-package-with-report server
