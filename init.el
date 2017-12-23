d0b222eac9a2ca7d5dee7b6884338975186106b5
yuwki0131 <yuwki0131@gmail.com>
2017-12-23 16:45:50 +0900

    fix archives repositories

--- init.el
+++ init.el
@@ -9,9 +9,10 @@
 (require 'package)
 
 (setq package-archives
-      '(("gnu"   . "http://elpa.gnu.org/packages/")
-        ;("melpa" . "http://melpa.milkbox.net/packages/")
-        ("org"   . "http://orgmode.org/elpa/")))
+      '(("melpa" . "https://melpa.org/packages/")
+        ("melpa-stable" . "https://stable.melpa.org/packages/")
+        ("marmalade" . "http://marmalade-repo.org/packages/")
+        ("org" . "http://orgmode.org/elpa/")))
 
 (package-initialize)
 
