0b136960deda693279615313aacda839845fdc44
yuwki0131 <yuwki0131@gmail.com>
2017-12-23 16:28:06 +0900

    purge melpa & add rust

--- init.el
+++ init.el
@@ -10,7 +10,7 @@
 
 (setq package-archives
       '(("gnu"   . "http://elpa.gnu.org/packages/")
-        ("melpa" . "http://melpa.milkbox.net/packages/")
+        ;("melpa" . "http://melpa.milkbox.net/packages/")
         ("org"   . "http://orgmode.org/elpa/")))
 
 (package-initialize)
