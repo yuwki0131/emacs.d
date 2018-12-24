b966bb236050c90fb4e05b0158f6f2b550bce786
yuwki0131 <yuwkixxx@gmail.com>
2018-12-24 12:18:58 +0900

    delete no-used no-installed config

--- config/key-binding.el
+++ config/key-binding.el
@@ -188,9 +188,6 @@
 (gssk-explain-function "キーバインド表示")
 (gssk-bind "C-z C-k" 'describe-bindings)
 
-(gssk-explain-function "コメント表示/非表示")
-(gssk-bind "C-z c"   'hide/show-comments-toggle)
-
 (gssk-subcategory "辞書")
 
 (gssk-explain-function "現在の単語の意味を表示(要辞書設定)")
