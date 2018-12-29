e6c1628bb780206e3f4d5255ddd70ffe19819d54
yuwki0131 <yuwkixxx@gmail.com>
2018-12-29 15:37:05 +0900

    js用のhookを追加

--- config/language-conf.el
+++ config/language-conf.el
@@ -222,6 +222,13 @@
 ;;; config : AltJavascripts
 ;;; --------------------------------------------------------------------------------
 
+(add-hook 'js2-mode-hook
+          (lambda ()
+            (setq my-js-mode-indent-num 2)
+            (setq js2-basic-offset my-js-mode-indent-num)
+            (setq js-switch-indent-offset my-js-mode-indent-num)
+            ))
+
 ;;; ---------------------------------------------------------------------------
 ;;; prolog-mode : Type Script (unchecked)
 ;;; ---------------------------------------------------------------------------
