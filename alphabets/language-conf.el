ee8cc5d50f48b117808502d4b7169e6ddda6256b
yuwki0131 <yuwki0131@gmail.com>
2019-09-16 00:08:40 +0900

    add tide

--- alphabets/language-conf.el
+++ alphabets/language-conf.el
@@ -251,6 +251,16 @@
       ))
   (add-hook 'ts-mode-hook 'my-ts-mode))
 
+(use-package-with-report tide
+  :config
+  (add-hook 'typescript-mode-hook
+            (lambda ()
+              (tide-setup)
+              (flycheck-mode t)
+              (setq flycheck-check-syntax-automatically '(save mode-enabled))
+              (eldoc-mode t)
+              (company-mode-on))))
+
 (use-package-with-report js2-mode
   :config
   (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
